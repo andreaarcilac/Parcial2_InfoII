4e94efd01a097e145db5c69aefd86baa2855011a
andreaarcilac <andrea.arcilac@udea.edu.co>
2026-04-25 18:41:08 -0500

    Definición de función para gráficos de columna SIATA

--- clases.py
+++ clases.py
@@ -17,3 +17,26 @@ class ArchivoSIATA:
         print(self.datos.describe())
         print("\n===== INFO DEL DATAFRAME =====")
         self.datos.info()
+    
+    def graficar_columna(self, columna):
+        fig, axes = plt.subplots(1, 3, figsize=(15, 4))
+
+        self.datos[columna].plot(ax=axes[0])
+        axes[0].set_title("Plot de " + columna)
+        axes[0].set_xlabel("Índice")
+        axes[0].set_ylabel(columna)
+
+        self.datos.boxplot(column=columna, ax=axes[1])
+        axes[1].set_title("Boxplot de " + columna)
+        axes[1].set_ylabel(columna)
+
+        self.datos[columna].hist(ax=axes[2], bins=20)
+        axes[2].set_title("Histograma de " + columna)
+        axes[2].set_xlabel(columna)
+        axes[2].set_ylabel("Frecuencia")
+
+        plt.tight_layout()
+        nombre_imagen = "graficos_siata_" + columna + ".png"
+        plt.savefig(nombre_imagen, dpi=150)
+        plt.show()
+        print("Grafico guardado como:", nombre_imagen)
